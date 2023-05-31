9f70180fc4d2f7a1abc5745c1ee1f005b9bd313b
Rutik Ravindra Kalokhe <rutik.ravindra-kalokhe@hpe.com>
2023-05-31 13:10:51 +0530

    API for 2 images, some minor changes in BL

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from fastapi import FastAPI, File
 from fastapi.middleware.cors import CORSMiddleware
+from identify import identify_human_image
 
 # Create a FastAPI application
 app = FastAPI()
@@ -15,76 +16,62 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Define the paths to cascade classifier XML files
-fullbody_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
-frontalface_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-upperbody_cascade = cv2.CascadeClassifier('haarcascade_upperbody.xml')
+# # Define the paths to cascade classifier XML files
+# fullbody_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+# frontalface_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# upperbody_cascade = cv2.CascadeClassifier('haarcascade_upperbody.xml')
 
-# Define a route for image classification for path 
-@app.get('/identify')
-async def identify_image(image_path: str):
-    # Read the image from the provided path
-    cv_image = cv2.imread(image_path)
+# # Define a route for image classification for path 
+# @app.get('/identify')
+# async def identify_image(image_path: str):
+#     # Read the image from the provided path
+#     cv_image = cv2.imread(image_path)
 
-    # Convert the image to grayscale
-    gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+#     # Convert the image to grayscale
+#     gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
 
-    # Detect humans using different cascade classifiers
-    fullbody_humans = fullbody_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-    frontalface_humans = frontalface_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-    upperbody_humans = upperbody_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+#     # Detect humans using different cascade classifiers
+#     fullbody_humans = fullbody_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+#     frontalface_humans = frontalface_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+#     upperbody_humans = upperbody_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    # Print the number of humans detected by each classifier
-    print("Full Body Humans: ", len(fullbody_humans))
-    print("Frontal Face Humans: ", len(frontalface_humans))
-    print("Upper Body Humans: ", len(upperbody_humans))
+#     # Print the number of humans detected by each classifier
+#     print("Full Body Humans: ", len(fullbody_humans))
+#     print("Frontal Face Humans: ", len(frontalface_humans))
+#     print("Upper Body Humans: ", len(upperbody_humans))
 
 
-     # Check if humans are detected using each cascade classifier
-    if ( len(fullbody_humans) > 0 and len(frontalface_humans) > 0 ) or (len(frontalface_humans) > 0 and len(upperbody_humans) > 0) or (len(upperbody_humans) > 0 and len(fullbody_humans) > 0):
-        class_label = 'Human'
-    else:
-        class_label = 'Non-Human'
+#      # Check if humans are detected using each cascade classifier
+#     if ( len(fullbody_humans) > 0 and len(frontalface_humans) > 0 ) or (len(frontalface_humans) > 0 and len(upperbody_humans) > 0) or (len(upperbody_humans) > 0 and len(fullbody_humans) > 0):
+#         class_label = 'Human'
+#     else:
+#         class_label = 'Non-Human'
 
-    # Prepare the response
-    response = {
-        'class_label': class_label,
-        'confidence': float(confidence)
-    }
+#     # Prepare the response
+#     response = {
+#         'class_label': class_label,
+#         'confidence': float(confidence)
+#     }
 
-    return response
+#     return response
 
 @app.post('/image')
-async def identify_image(image: bytes = File(...)):
+async def identify_single_image(image: bytes = File(...)):
 
-    # Convert the image bytes to a NumPy array
-    nparr = np.frombuffer(image, np.uint8)
+    return identify_human_image(image)
 
-    # Read the image from the NumPy array
-    cv_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    # Convert the image to grayscale
-    gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+@app.post('/two_image')
+async def identify_two_images(image1: bytes = File(...), image2: bytes = File(...)):
 
-    # Detect humans using different cascade classifiers
-    fullbody_humans = fullbody_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-    frontalface_humans = frontalface_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-    upperbody_humans = upperbody_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    resp1 = identify_human_image(image1)
 
-    # Print the number of humans detected by each classifier
-    print("Full Body Humans: ", len(fullbody_humans))
-    print("Frontal Face Humans: ", len(frontalface_humans))
-    print("Upper Body Humans: ", len(upperbody_humans))
+    resp2 = identify_human_image(image2)
 
-    # Check if humans are detected using each cascade classifier
-    if ( len(fullbody_humans) > 0 and len(frontalface_humans) > 0 ) or (len(frontalface_humans) > 0 and len(upperbody_humans) > 0) or (len(upperbody_humans) > 0 and len(fullbody_humans) > 0):
-        class_label = 'Human'
-    else:
-        class_label = 'Non-Human'
-
-    # Prepare the response
     response = {
-        'class_label': class_label
+        "image1" : resp1,
+        "image2" : resp2
     }
 
+    print(response)
     return response
