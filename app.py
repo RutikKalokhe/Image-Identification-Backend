9874f0536eb19d613a4712c5a1c37d9fd3db69c5
Rutik Ravindra Kalokhe <rutik.ravindra-kalokhe@hpe.com>
2023-06-01 21:08:12 +0530

    MongoDB connection, API for feedback

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-import cv2
-import numpy as np
-from fastapi import FastAPI, File
+from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-from identify import identify_human_image
+from identify import identify_human_image, change_identity
 
 # Create a FastAPI application
 app = FastAPI()
@@ -16,64 +14,26 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# # Define the paths to cascade classifier XML files
-# fullbody_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
-# frontalface_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# upperbody_cascade = cv2.CascadeClassifier('haarcascade_upperbody.xml')
-
-# # Define a route for image classification for path 
-# @app.get('/identify')
-# async def identify_image(image_path: str):
-#     # Read the image from the provided path
-#     cv_image = cv2.imread(image_path)
-
-#     # Convert the image to grayscale
-#     gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-
-#     # Detect humans using different cascade classifiers
-#     fullbody_humans = fullbody_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-#     frontalface_humans = frontalface_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-#     upperbody_humans = upperbody_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-#     # Print the number of humans detected by each classifier
-#     print("Full Body Humans: ", len(fullbody_humans))
-#     print("Frontal Face Humans: ", len(frontalface_humans))
-#     print("Upper Body Humans: ", len(upperbody_humans))
-
-
-#      # Check if humans are detected using each cascade classifier
-#     if ( len(fullbody_humans) > 0 and len(frontalface_humans) > 0 ) or (len(frontalface_humans) > 0 and len(upperbody_humans) > 0) or (len(upperbody_humans) > 0 and len(fullbody_humans) > 0):
-#         class_label = 'Human'
-#     else:
-#         class_label = 'Non-Human'
-
-#     # Prepare the response
-#     response = {
-#         'class_label': class_label,
-#         'confidence': float(confidence)
-#     }
-
-#     return response
-
-
 @app.get('/')
 async def home():
-    
     return "Home Page"
 
-
 @app.post('/image')
-async def identify_single_image(image: bytes = File(...)):
+async def identify_single_image(image: UploadFile = File(...)):
 
-    return identify_human_image(image)
+    image_bytes = await image.read()
+
+    filename = image.filename
+
+    return identify_human_image(filename, image_bytes)
 
 
 @app.post('/two_image')
 async def identify_two_images(image1: bytes = File(...), image2: bytes = File(...)):
 
-    resp1 = identify_human_image(image1)
+    resp1 = identify_human_image("aaaa", image1)
 
-    resp2 = identify_human_image(image2)
+    resp2 = identify_human_image( "aaaa", image2)
 
     response = {
         "image1" : resp1,
@@ -82,3 +42,10 @@ async def identify_two_images(image1: bytes = File(...), image2: bytes = File(..
 
     print(response)
     return response
+
+
+@app.post("/feedback")
+async def feedback(file: str, new_identity: str):
+    
+    return change_identity(file, new_identity) 
+
--- identify.py
+++ identify.py
@@ -1,41 +1,78 @@
 import cv2
 import numpy as np
-from fastapi import File
+from fastapi import File, UploadFile 
+from pymongo import MongoClient
+
+
+# Connect to the MongoDB client
+client = MongoClient("mongodb://localhost:27017")
+db = client["mydatabse"]
+collection = db["test"]
 
 # Define the paths to cascade classifier XML files
 fullbody_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
 frontalface_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 upperbody_cascade = cv2.CascadeClassifier('haarcascade_upperbody.xml')
 
-def identify_human_image(image: bytes = File(...)):
-    # Convert the image bytes to a NumPy array
-    nparr = np.frombuffer(image, np.uint8)
+def identify_human_image( filename: str, image_bytes: bytes  = File(...)):
 
-    # Read the image from the NumPy array
-    cv_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    is_document_present = collection.find_one({"filename": filename})
 
-    # Convert the image to grayscale
-    gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+    print(is_document_present)
 
-    # Detect humans using different cascade classifiers
-    fullbody_humans = fullbody_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-    frontalface_humans = frontalface_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-    upperbody_humans = upperbody_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    if(is_document_present):
+        identity = is_document_present['identity']
 
-    # Print the number of humans detected by each classifier
-    print("Full Body Humans: ", len(fullbody_humans))
-    print("Frontal Face Humans: ", len(frontalface_humans))
-    print("Upper Body Humans: ", len(upperbody_humans))
+         # Prepare the response
+        response = {
+            'identity': identity
+        }
 
-    # Check if humans are detected using each cascade classifier
-    if ( len(fullbody_humans) > 0 and len(frontalface_humans) > 0 ) or (len(frontalface_humans) > 0 and len(upperbody_humans) > 0) or (len(upperbody_humans) > 0 and len(fullbody_humans) > 0):
-        identity = 'Human'
+        return response
     else:
-        identity = 'Non-Human'
 
-    # Prepare the response
-    response = {
-        'identity': identity
-    }
+        # Convert the image bytes to a NumPy array
+        nparr = np.frombuffer(image_bytes, np.uint8)
 
-    return response
+        # Read the image from the NumPy array
+        cv_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+        # Convert the image to grayscale
+        gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+
+        # Detect humans using different cascade classifiers
+        fullbody_humans = fullbody_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        frontalface_humans = frontalface_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        upperbody_humans = upperbody_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+        # Print the number of humans detected by each classifier
+        print("Full Body Humans: ", len(fullbody_humans))
+        print("Frontal Face Humans: ", len(frontalface_humans))
+        print("Upper Body Humans: ", len(upperbody_humans))
+
+        # Check if humans are detected using each cascade classifier
+        if ( len(fullbody_humans) > 0 and len(frontalface_humans) > 0 ) or (len(frontalface_humans) > 0 and len(upperbody_humans) > 0) or (len(upperbody_humans) > 0 and len(fullbody_humans) > 0):
+            identity = 'Human'
+        else:
+            identity = 'Non-Human'
+
+        # Prepare the response
+        response = {
+            'identity': identity
+        }
+
+        inserting_data = {"filename": filename, "identity": identity}
+
+        collection.insert_one(inserting_data)
+
+        return response
+
+
+def change_identity(filename: str, new_identity: str):
+    filter_query = {"filename": filename}
+
+    query = {"$set": {"identity": new_identity}}
+
+    collection.update_one(filter_query, query)
+
+    return "changes updated"
