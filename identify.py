c99906d215a34bbf9c420299359aa4b25bcd140c
RutikKalokhe <rutikkalokhe3@gmail.com>
2023-06-06 08:37:15 +0000

    added more cascade files

--- identify.py
+++ identify.py
@@ -14,6 +14,13 @@ fullbody_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
 frontalface_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 upperbody_cascade = cv2.CascadeClassifier('haarcascade_upperbody.xml')
 
+
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+eyeglass_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+profileface_cascade = cv2.CascadeClassifier('haarcascade_profileface.xml')
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
+
+
 def identify_human_image( filename: str, image_bytes: bytes  = File(...)):
 
     # is_document_present = collection.find_one({"filename": filename})
@@ -45,16 +52,32 @@ def identify_human_image( filename: str, image_bytes: bytes  = File(...)):
         frontalface_humans = frontalface_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
         upperbody_humans = upperbody_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
+
+        eye_humans = eye_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        eye_glass_humans = eyeglass_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        profileface_humans = profileface_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        smile_humans = smile_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
         # Print the number of humans detected by each classifier
         print("Full Body Humans: ", len(fullbody_humans))
         print("Frontal Face Humans: ", len(frontalface_humans))
         print("Upper Body Humans: ", len(upperbody_humans))
 
+
+        print("Eye Humans: ", len(eye_humans))
+        print("Eye Glass Humans: ", len(eye_glass_humans))
+        print("Profile Face Humans: ", len(profileface_humans))
+        print("Smile Humans: ", len(smile_humans))
+
+
         # Check if humans are detected using each cascade classifier
         if ( len(fullbody_humans) > 0 and len(frontalface_humans) > 0 ) or (len(frontalface_humans) > 0 and len(upperbody_humans) > 0) or (len(upperbody_humans) > 0 and len(fullbody_humans) > 0):
             identity = 'Human'
         else:
-            identity = 'Non-Human'
+            if(len(frontalface_humans) > 0 and len(eye_humans) > 0 and len(eye_glass_humans) > 0 and len(profileface_humans) > 0):
+                identity = 'Human'
+            else:
+                identity = 'Non-Human'
 
         # Prepare the response
         response = {
